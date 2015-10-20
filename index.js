e6e91f7ec94b89f3b17499f9f9d1a5b487cd5736
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2015-10-20 17:21:05 +0200

    Informe general de rondas

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ var rondas_realizadas_router = require('./lib/rondas_realizadas/rondas_realizada
 var grupos_router = require('./lib/grupos/grupos_controller');
 var edificios_router = require('./lib/edificios/edificios_controller');
 var descargas_router = require('./lib/descargas/descargas_controller');
+var informes_router = require('./lib/informes/informes_controller');
 
 // read app parameters (host and port for the API)
 var config = require('./config.json');
@@ -83,6 +84,7 @@ app.use('/api/rondas-realizadas', rondas_realizadas_router);
 app.use('/api/grupos', grupos_router);
 app.use('/api/edificios', edificios_router);
 app.use('/api/descargas', descargas_router);
+app.use('/api/informes', informes_router);
 
 
 // -- start server
