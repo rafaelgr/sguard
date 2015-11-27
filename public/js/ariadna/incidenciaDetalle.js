639d9bcb7fc315fa1a8ac307c0245383af3d6a00
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2015-11-27 08:54:44 +0100

    Mantenimiento de incidencias

---
+++ public/js/ariadna/incidenciaDetalle.js
@@ -0,0 +1,114 @@
+﻿/*-------------------------------------------------------------------------- 
+incidenciaDetalle.js
+Funciones js par la página IncidenciaDetalle.html
+---------------------------------------------------------------------------*/
+var adminId = 0;
+
+function initForm() {
+    comprobarLogin();
+    // de smart admin
+    pageSetUp();
+    // 
+    getVersionFooter();
+    vm = new admData();
+    ko.applyBindings(vm);
+    // asignación de eventos al clic
+    $("#btnAceptar").click(aceptar());
+    $("#btnSalir").click(salir());
+    $("#frmIncidencia").submit(function() {
+        return false;
+    });
+
+    adminId = gup('IncidenciaId');
+    if (adminId != 0) {
+        var data = {
+                incidenciaId: adminId
+            }
+            // hay que buscar ese elemento en concreto
+        $.ajax({
+            type: "GET",
+            url: myconfig.apiUrl + "/api/incidencias/" + adminId,
+            dataType: "json",
+            contentType: "application/json",
+            data: JSON.stringify(data),
+            success: function(data, status) {
+                // hay que mostrarlo en la zona de datos
+                loadData(data);
+            },
+            error: errorAjax
+        });
+    } else {
+        // se trata de un alta ponemos el id a cero para indicarlo.
+        vm.incidenciaId(0);
+    }
+}
+
+function admData() {
+    var self = this;
+    self.incidenciaId = ko.observable();
+    self.nombre = ko.observable();
+}
+
+function loadData(data) {
+    vm.incidenciaId(data.incidenciaId);
+    vm.nombre(data.nombre);
+}
+
+function datosOK() {
+    return $('#frmIncidencia').valid();
+}
+
+function aceptar() {
+    var mf = function() {
+        if (!datosOK())
+            return;
+        var data = {
+            incidencia: {
+                "incidenciaId": vm.incidenciaId(),
+                "nombre": vm.nombre()
+            }
+        };
+        if (adminId == 0) {
+            $.ajax({
+                type: "POST",
+                url: myconfig.apiUrl + "/api/incidencias",
+                dataType: "json",
+                contentType: "application/json",
+                data: JSON.stringify(data),
+                success: function(data, status) {
+                    // hay que mostrarlo en la zona de datos
+                    loadData(data);
+                    // Nos volvemos al general
+                    var url = "IncidenciaGeneral.html?IncidenciaId=" + vm.incidenciaId();
+                    window.open(url, '_self');
+                },
+                error: errorAjax
+            });
+        } else {
+            $.ajax({
+                type: "PUT",
+                url: myconfig.apiUrl + "/api/incidencias/" + adminId,
+                dataType: "json",
+                contentType: "application/json",
+                data: JSON.stringify(data),
+                success: function(data, status) {
+                    // hay que mostrarlo en la zona de datos
+                    loadData(data);
+                    // Nos volvemos al general
+                    var url = "IncidenciaGeneral.html?IncidenciaId=" + vm.incidenciaId();
+                    window.open(url, '_self');
+                },
+                error: errorAjax
+            });
+        }
+    };
+    return mf;
+}
+
+function salir() {
+    var mf = function() {
+        var url = "IncidenciaGeneral.html";
+        window.open(url, '_self');
+    };
+    return mf;
+}
---
+++ public/js/ariadna/incidenciaGeneral.js
@@ -0,0 +1,213 @@
+﻿/*-------------------------------------------------------------------------- 
+incidenciaGeneral.js
+Funciones js par la página IncidenciaGeneral.html
+
+---------------------------------------------------------------------------*/
+var responsiveHelper_dt_basic = undefined;
+var responsiveHelper_datatable_fixed_column = undefined;
+var responsiveHelper_datatable_col_reorder = undefined;
+var responsiveHelper_datatable_tabletools = undefined;
+
+var dataIncidencias;
+var incidenciaId;
+
+var breakpointDefinition = {
+    tablet: 1024,
+    phone: 480
+};
+
+
+function initForm() {
+    comprobarLogin();
+    // de smart admin
+    pageSetUp();
+    getVersionFooter();
+    //
+    $('#btnBuscar').click(buscarIncidencias());
+    $('#btnAlta').click(crearIncidencia());
+    $('#frmBuscar').submit(function () {
+        return false
+    });
+    //$('#txtBuscar').keypress(function (e) {
+    //    if (e.keyCode == 13)
+    //        buscarIncidencias();
+    //});
+    //
+    initTablaIncidencias();
+    // comprobamos parámetros
+    incidenciaId = gup('IncidenciaId');
+    if (incidenciaId !== '') {
+        // cargar la tabla con un único valor que es el que corresponde.
+        var data = {
+            id: incidenciaId
+        }
+        // hay que buscar ese elemento en concreto
+        $.ajax({
+            type: "GET",
+            url: myconfig.apiUrl + "/api/incidencias/" + incidenciaId,
+            dataType: "json",
+            contentType: "application/json",
+            data: JSON.stringify(data),
+            success: function (data, status) {
+                // hay que mostrarlo en la zona de datos
+                var data2 = [data];
+                loadTablaIncidencias(data2);
+            },
+            error: errorAjax
+        });
+    }
+}
+
+function initTablaIncidencias() {
+    tablaCarro = $('#dt_incidencia').dataTable({
+        autoWidth: true,
+        preDrawCallback: function () {
+            // Initialize the responsive datatables helper once.
+            if (!responsiveHelper_dt_basic) {
+                responsiveHelper_dt_basic = new ResponsiveDatatablesHelper($('#dt_incidencia'), breakpointDefinition);
+            }
+        },
+        rowCallback: function (nRow) {
+            responsiveHelper_dt_basic.createExpandIcon(nRow);
+        },
+        drawCallback: function (oSettings) {
+            responsiveHelper_dt_basic.respond();
+        },
+        language: {
+            processing: "Procesando...",
+            info: "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            infoEmpty: "Mostrando registros del 0 al 0 de un total de 0 registros",
+            infoFiltered: "(filtrado de un total de _MAX_ registros)",
+            infoPostFix: "",
+            loadingRecords: "Cargando...",
+            zeroRecords: "No se encontraron resultados",
+            emptyTable: "Ningún dato disponible en esta tabla",
+            paginate: {
+                first: "Primero",
+                previous: "Anterior",
+                next: "Siguiente",
+                last: "Último"
+            },
+            aria: {
+                sortAscending: ": Activar para ordenar la columna de manera ascendente",
+                sortDescending: ": Activar para ordenar la columna de manera descendente"
+            }
+        },
+        data: dataIncidencias,
+        columns: [{
+            data: "nombre"
+        }, {
+            data: "incidenciaId",
+            render: function (data, type, row) {
+                var bt1 = "<button class='btn btn-circle btn-danger btn-lg' onclick='deleteIncidencia(" + data + ");' title='Eliminar registro'> <i class='fa fa-trash-o fa-fw'></i> </button>";
+                var bt2 = "<button class='btn btn-circle btn-success btn-lg' onclick='editIncidencia(" + data + ");' title='Editar registro'> <i class='fa fa-edit fa-fw'></i> </button>";
+                var html = "<div class='pull-right'>" + bt1 + " " + bt2 + "</div>";
+                return html;
+            }
+        }]
+    });
+}
+
+function datosOK() {
+    //TODO: Incluir en la validación si el certificado figura en el almacén de certificados.
+    $('#frmBuscar').validate({
+        rules: {
+            txtBuscar: { required: true },
+        },
+        // Messages for form validation
+        messages: {
+            txtBuscar: {
+                required: 'Introduzca el texto a buscar'
+            }
+        },
+        // Do not change code below
+        errorPlacement: function (error, element) {
+            error.insertAfter(element.parent());
+        }
+    });
+    return $('#frmBuscar').valid();
+}
+
+function loadTablaIncidencias(data) {
+    var dt = $('#dt_incidencia').dataTable();
+    if (data !== null && data.length === 0) {
+        mostrarMensajeSmart('No se han encontrado registros');
+        $("#tbIncidencia").hide();
+    } else {
+        dt.fnClearTable();
+        dt.fnAddData(data);
+        dt.fnDraw();
+        $("#tbIncidencia").show();
+    }
+}
+
+function buscarIncidencias() {
+    var mf = function () {
+        if (!datosOK()) {
+            return;
+        }
+        // obtener el n.serie del certificado para la firma.
+        var aBuscar = $('#txtBuscar').val();
+        // enviar la consulta por la red (AJAX)
+        $.ajax({
+            type: "GET",
+            url: myconfig.apiUrl + "/api/incidencias/?nombre=" + aBuscar,
+            dataType: "json",
+            contentType: "application/json",
+            success: function (data, status) {
+                // hay que mostrarlo en la zona de datos
+                loadTablaIncidencias(data);
+            },
+            error: errorAjax
+        });
+    };
+    return mf;
+}
+
+function crearIncidencia() {
+    var mf = function () {
+        var url = "IncidenciaDetalle.html?IncidenciaId=0";
+        window.open(url, '_self');
+    };
+    return mf;
+}
+
+function deleteIncidencia(id) {
+    // mensaje de confirmación
+    var mens = "¿Realmente desea borrar este registro?";
+    $.SmartMessageBox({
+        title: "<i class='fa fa-info'></i> Mensaje",
+        content: mens,
+        buttons: '[Aceptar][Cancelar]'
+    }, function (ButtonPressed) {
+        if (ButtonPressed === "Aceptar") {
+            var data = {
+                incidenciaId: id
+            };
+            $.ajax({
+                type: "DELETE",
+                url: myconfig.apiUrl + "/api/incidencias/" + id,
+                dataType: "json",
+                contentType: "application/json",
+                data: JSON.stringify(data),
+                success: function (data, status) {
+                    var fn = buscarIncidencias();
+                    fn();
+                },
+                error: errorAjax
+            });
+        }
+        if (ButtonPressed === "Cancelar") {
+            // no hacemos nada (no quiere borrar)
+        }
+    });
+}
+
+function editIncidencia(id) {
+    // hay que abrir la página de detalle de incidencia
+    // pasando en la url ese ID
+    var url = "IncidenciaDetalle.html?IncidenciaId=" + id;
+    window.open(url, '_self');
+}
+
+
