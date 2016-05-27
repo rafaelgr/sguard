fabc3e75e2d4cc7c1937be2cb3afdb07a8c3f5c8
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2016-05-27 13:17:45 +0200

    VRS 2.5.1
    Control de rondas incluidos puntos correcto

--- sql/sguard_2.5.sql
+++ sql/sguard_2.5.sql
@@ -1,4 +1,4 @@
 ALTER TABLE `sguard`.`rondas`   
 ADD COLUMN `csnmax` INT(11) NULL  COMMENT 'Máximo tiempo entre dos inicios de la misma ronda' AFTER `maxtime`,
-ADD COLUMN `csnmargen` INT(11) NULL  COMMENT 'Margen antes de considerar incorrecto' AFTER `csmax`,
+ADD COLUMN `csnmargen` INT(11) NULL  COMMENT 'Margen antes de considerar incorrecto' AFTER `csnmax`,
 ADD COLUMN `lastcontrol` DATETIME NULL  COMMENT 'Última vez que se inició un control de esta ronda' AFTER `csnmargen`;
