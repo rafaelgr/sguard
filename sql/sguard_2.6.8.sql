00b1a33af70adc15bdf055432b981461edc9797b
Rafa <rafa@myariadna.com>
2016-12-19 18:56:37 +0100

    Actualización de descargas preparada

---
+++ sql/sguard_2.6.8.sql
@@ -0,0 +1,5 @@
+ALTER TABLE `sguard`.`descargas`   
+  ADD COLUMN `procesada` BOOL DEFAULT FALSE NULL AFTER `resultado`;
+ALTER TABLE `sguard`.`rondas_realizadas`   
+  ADD COLUMN `descargaId` INT(11) NULL AFTER `observaciones`,
+  ADD CONSTRAINT `ref_descarga` FOREIGN KEY (`descargaId`) REFERENCES `sguard`.`descargas`(`descargaId`);
